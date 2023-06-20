f510035c81468d431e6332d86decbf93c19ee6ec
Vondert <25042018avic@gmail.com>
2023-06-20 18:54:33 +0300

    Registration added

--- DailyFoodProject/Models/User.cs
+++ DailyFoodProject/Models/User.cs
@@ -5,43 +5,15 @@ using System.Text;
 public class User : Person{
 
 
-    public string login { get; set; }
+    public string login { get; set; } = string.Empty;
 
-    public string password { get; set; }
-    public string phoneNumber { get; set; }
+    public string password { get; set; } = string.Empty;
+    public string phoneNumber { get; set; } = string.Empty;
 
-    public int bonuses { get; set; } = 0;
+    public DateTime dob { get; set; } = DateTime.Now;
+    public double bonuses { get; set; } = 0;
 
 
     public bool status { get; set; } = true;
 
-
-
-    public bool IsValid(string str)
-    {
-        str += login;
-        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(str);
-        request.Method = "GET";
-
-        try
-        {
-            //Отправляем запрос и получаем ответ
-            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-            {
-                // Читаем содержимое ответа
-                using (var stream = response.GetResponseStream())
-                using (var reader = new System.IO.StreamReader(stream))
-                {
-                    string responseText = reader.ReadToEnd();
-                    return Convert.ToBoolean(responseText);
-                }
-                
-            }
-        }
-        catch (WebException ex)
-        {
-            Console.WriteLine("Ошибка выполнения запроса: " + ex.Message);
-            return true;
-        }
-    }
 }
